8ad480fc8395e3540dd9be0ac5ed5ac777aa64a2
Mahsa <haghnevismahsa@gmail.com>
2024-08-20 09:23:50 +0330

    made api file and added my key

---
+++ NetflixApp/Managers/APICaller.swift
@@ -0,0 +1,18 @@
+//
+//  APICaller.swift
+//  NetflixApp
+//
+//  Created by Mahsa on 8/19/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_Key =  "1f25a6c2e1de8367669d6b96d7a74ce0"
+}
+
+
+class APICaller {
+    static let shared = APICaller()
+    
+}
